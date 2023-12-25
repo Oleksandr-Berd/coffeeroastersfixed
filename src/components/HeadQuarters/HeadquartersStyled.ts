a7dda1da6e257fe72de9c087872710e498faf63f
Oleksandr-Berd <alex.berd86@gmail.com>
2023-12-25 12:37:59 +0200

    made headquarters section tab

--- src/components/HeadQuarters/HeadquartersStyled.ts
+++ src/components/HeadQuarters/HeadquartersStyled.ts
@@ -1,62 +1,92 @@
 import styled from "@emotion/styled";
 
 type StyledProps = {
-    img:string
-}
-
+  img: string;
+};
 
 export const CommonCon = styled.div`
-margin-bottom: 120px;
+  margin-bottom: 120px;
 
-text-align: center;
-`
+  text-align: center;
+
+  @media (min-width: 768px) {
+    margin-bottom: 144px;
+
+    text-align: left;
+
+    & > ul {
+      display: flex;
+      flex-direction: row;
+      align-items: flex-start;
+      justify-content: space-between;
+    }
+  }
+`;
 
 export const Title = styled.h2`
-margin-bottom: 72px;
+  margin-bottom: 72px;
 
-font-family: "Frau";
-font-size: 24px;
-line-height: 1.33;
-color: #83888F;
-`
+  font-family: "Frau";
+  font-size: 24px;
+  line-height: 1.33;
+  color: #83888f;
+`;
 
 export const Item = styled.li<StyledProps>`
+  text-align: center;
 
-text-align: center;
-
-&:not(:last-child){
+  &:not(:last-child) {
     margin-bottom: 80px;
-}
+  }
 
+  @media (min-width: 768px) {
+    text-align: left;
 
+    &:not(:last-child) {
+      margin-bottom: 0;
+      margin-right: 21px;
+    }
+  }
 
-&::before{
+  &::before {
     display: inline-block;
-    content: url(${props => props.img});
+    content: url(${(props) => props.img});
 
     margin-bottom: 48px;
-}
 
-& > h3{
+    @media (min-width: 768px) {
+      height: 50px;
+    }
+  }
+
+  & > h3 {
     margin-bottom: 22px;
 
     font-family: "Frau";
     font-size: 28px;
     line-height: 1.3;
-    color: #333D4B;
-}
+    color: #333d4b;
 
-& > address{
-width: 160px;
+    @media (min-width: 768px) {
+      font-size: 24px;
+      line-height: 1.5;
+    }
+  }
 
-margin-left: auto;
-margin-right: auto;
+  & > address {
+    width: 160px;
+
+    margin-left: auto;
+    margin-right: auto;
 
     font-family: "Bar";
     font-style: normal;
     font-size: 16px;
     line-height: 1.75;
-    color: #333D4B;
-    
-}
-`
+    color: #333d4b;
+
+    @media (min-width: 768px) {
+      margin: 0;
+    }
+  }
+`;
