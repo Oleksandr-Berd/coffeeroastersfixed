af675b4d5f4775cbbf03fdbb174fb9bd3933f622
Oleksandr-Berd <alex.berd86@gmail.com>
2023-12-26 08:15:10 +0200

    smth went wrong

--- src/ui/SocNet/SocNetStyled.ts
+++ src/ui/SocNet/SocNetStyled.ts
@@ -11,11 +11,9 @@ export const SocNetBar = styled.nav`
   }
 
   @media (min-width: 1440px) {
-
     & img:hover {
       filter: brightness(0) invert(1) sepia(100%) saturate(500%)
         hue-rotate(0deg) grayscale(0) contrast(0.5);
-
     }
   }
 `;
