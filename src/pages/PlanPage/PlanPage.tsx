6923f12ded072ab22c10285a1d9d3788e4f3dec7
Oleksandr-Berd <alex.berd86@gmail.com>
2023-12-26 15:55:11 +0200

    fixing deploy

--- src/pages/PlanPage/PlanPage.tsx
+++ src/pages/PlanPage/PlanPage.tsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
 
 import Manual from "../../components/Manual/Manual";
 import Order from "../../components/Order/Order";
 import PlanHero from "../../components/PlanHero/PlanHero";
 import { OrderQuestion, OrderSummary } from "../../utils/types/types";
-import { setkeyName } from "../../utils/helpers";
 import * as SC from './PlanPageStyled'
 
 import Summary from "../../components/Summary/Summary";
